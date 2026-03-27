0280eec7c345c19b13155a12102b65a63b2a30c0
pakrc194 <pakrc194@gmail.com>
2026-03-27 17:37:57 +0900

    commit update cors 2

--- server/src/main/java/com/example/github/cors/CorsConfiguration.java
+++ server/src/main/java/com/example/github/cors/CorsConfiguration.java
@@ -7,10 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer{
 	
+	@Value("${ALLOWED_ORIGINS}")
+	private String allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOriginPatterns("*")  // 모든 출처 허용
+			.allowedOrigins(allowedOrigins.split(","))
             .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
             .allowCredentials(true);
     }
