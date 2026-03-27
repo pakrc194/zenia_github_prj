75eda88ce22cee049769beb864ed2123f800620e
pakrc194 <pakrc194@gmail.com>
2026-03-27 17:30:28 +0900

    commit update cors

--- client/src/lib/axiosInstance.ts
+++ client/src/lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios, {type AxiosInstance} from 'axios';
 
 const api: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:8080/', // 스프링 부트 서버 주소
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/',
   timeout: 5000, // 5초 안에 응답 없으면 타임아웃
   headers: {
     'Content-Type': 'application/json',
--- server/src/main/java/com/example/github/cors/CorsConfiguration.java
+++ server/src/main/java/com/example/github/cors/CorsConfiguration.java
@@ -7,12 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer{
 	
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-        .allowedOrigins("http://localhost:5173")
-        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-		.allowCredentials(true);
-	}
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")  // 모든 출처 허용
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+            .allowCredentials(true);
+    }
 
 }
