06deaf52e6db36da8ef80ead7647f0df06ab5c66
pakrc194 <pakrc194@gmail.com>
2026-03-30 16:11:38 +0900

    commit revert2

---
+++ client/src/components/Statistics.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Statistics() {
+  return (
+    <>
+        <h1>Statistics</h1>
+        <div className='chart_wrapper'>
+            <div className='line_chart'>
+                
+
+            </div>
+        </div>
+    </>
+    15c30a7
+  )
+}
